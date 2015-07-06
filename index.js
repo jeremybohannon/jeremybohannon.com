e627e16517d1f56a7cabeb5fe3663bd60bb46c3a
JeremyBoh <jeremybohannon@outlook.com>
2015-07-06 19:16:42 -0400

    Re-coded inner-container for computability.  Added a folder for CSS styles.

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.get('/contact/', function (req, res) {
   res.sendFile(path.join(__dirname + '/contact.html'));
 });
 
-app.get('/stylesheet.css', function(req, res) {
-  res.sendFile(__dirname + '/stylesheet.css');
+app.get('/styles/stylesheet.css', function(req, res) {
+  res.sendFile(__dirname + '/styles/stylesheet.css');
 });
 
-app.get('/contactForm.css', function(req, res) {
-  res.sendFile(__dirname + '/contactForm.css');
+app.get('/styles/contactForm.css', function(req, res) {
+  res.sendFile(__dirname + '/styles/contactForm.css');
 });
 
 app.get('/favicon/tempIcon.ico', function(req, res) {

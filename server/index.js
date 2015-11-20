7fb676a85d8d5add193488cc3006cf9ccda892dc
JeremyBoh <jeremybohannon@outlook.com>
2015-11-20 01:59:18 -0500

    Added resume link @ /resume.pdf

--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ app.get('/kristinhorton', function(req, res){
 	res.sendFile(path.join(__dirname + '../../kristinhorton.com/index.html'));
 });
 
+app.get('/resume.pdf', function(req, res){
+	res.sendFile(path.join(__dirname + '../../client/resources/resume.pdf'));
+});
+
 app.use(function (req, res, next) {
 	res.status(404).send('Error: 404');
 });

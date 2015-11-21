5b3905cf8b0a5829f70e1a1d6ba053fea9908d7d
JeremyBoh <jeremybohannon@outlook.com>
2015-11-20 21:36:10 -0500

    server test

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
 	res.status(404).send('Error: 404');
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(80, function () {
 
     var host = server.address().address;
     var port = server.address().port;

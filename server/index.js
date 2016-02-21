23f89e0c68585f1c64f62da8130ddacf21f85b0c
JeremyBoh <jeremybohannon@outlook.com>
2016-02-20 22:30:01 -0500

    Updated server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ var app = express();
 var path = require("path");
 var http = require('http');
 
+var client = path.join(__dirname, "../client/");
+
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname + '../../client/build/index.html'));
 });
@@ -16,17 +18,11 @@ app.get('/playground', function(req, res){
 	res.sendFile(path.join(__dirname + '../../client/build/playground.html'));
 });
 
-app.use('/client',        express.static(path.join(__dirname + '../../client')));
-
-app.use('/build',         express.static(path.join(__dirname + '../../client/build')));
-
-app.use('/styles',        express.static(path.join(__dirname + '../../client/styles')));
-
-app.use('/resources',     express.static(path.join(__dirname + '../../client/resources')));
-
-app.use('/kristinhorton', express.static(path.join(__dirname + '../../kristinhorton.com')));
+app.use('/client',        express.static(client));
 
 app.use('/sampulator',    express.static(path.join(__dirname + '../../sampulator')));
+
+app.use('/kristinhorton', express.static(path.join(__dirname + '../../kristinhorton.com')));
 
 app.use(function (req, res, next) {
 	res.status(404).send('Error: 404');
@@ -38,5 +34,4 @@ var server = app.listen(80, function () {
     var port = server.address().port;
 
     console.log('My site is listening at http://%s:%s', host, port);
-
 });
